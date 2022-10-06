2e7855bb940a7e3bb9564df25ab3db809eb7fc83
1rlan <irlanidzzze@gmail.com>
2022-10-06 21:17:55 +0300

    program.c edit

--- program.c
+++ program.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void input(int *array, int size) {
+void input(int *array, int size, int change, int counter) {
     for (int i = 0; i < size; ++i) {
         scanf("%d", &array[i]);
+        if (array[i] != change) {
+            ++counter;
+        }
     }
 }
 
-void make_new_array(int* old_array, int *new_array, int size) {
-    for (int i = 0, j = 0; i < (size + 1) / 2 && j < size; ++i) {
-        new_array[i] = old_array[j];
-        j += 2;
+void make_new_array(int* old_array, int *new_array, int size, int change) {
+    int index = -1;
+    for (int i = 0; i < change; ++i) {
+        if (old_array[i] != change) {
+            ++index;
+            new_array[index] = old_array[i];
+        }
     }
-    for (int i = (size + 1) / 2, j = 1; i < size && j < size; ++i) {
-        new_array[i] = old_array[j];
-        j += 2;
-    }
+
 }
 
 void output(int *array, int size) {
     for (int i = 0; i < size; i++) {
         printf("%d", array[i]);
-        if (i != size - 1) {
-            printf(" ");
-        } else {
-            printf("\n");
-        }
+        printf(" ");
     }
+    printf("\n");
 }
 
 int main() {
-    int n;
+    int n, change, counter;
     scanf("%d", &n);
-
+    scanf("%d", &change);
     int old_array[n];
-    int new_array[n];
-
-    input(old_array, n);
-    make_new_array(old_array, new_array, n);
+    input(old_array, n, change, counter);
+    int new_array[counter];
+    make_new_array(old_array, new_array, n, change);
     output(new_array, n);
 
     return 0;
