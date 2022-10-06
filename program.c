0d9c0e54aae615b1e71f32839bbf296725830e1a
1rlan <irlanidzzze@gmail.com>
2022-10-06 21:45:50 +0300

    program.c edit

--- program.c
+++ program.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void input(int *array, int size, int change, int counter) {
+int input(int *array, int size, int notvalid) {
+    int counter = 0;
     for (int i = 0; i < size; ++i) {
         scanf("%d", &array[i]);
-        if (array[i] != change) {
+        if (array[i] != notvalid) {
             ++counter;
         }
     }
+    return counter;
 }
 
-void make_new_array(int* old_array, int *new_array, int size, int change) {
+void make_new_array(int* old_array, int *new_array, int size, int notvalid) {
     int index = -1;
     for (int i = 0; i < size; ++i) {
-        if (old_array[i] != change) {
+        if (old_array[i] != notvalid) {
             ++index;
             new_array[index] = old_array[i];
         }
     }
-
 }
 
 void output(int *array, int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d", array[i]);
-        printf(" ");
+        printf("%d ", array[i]);
     }
-    printf("\n");
 }
 
 int main() {
-    int n, change, counter;
-    scanf("%d", &n);
-    scanf("%d", &change);
-    int old_array[n];
-
-    input(old_array, n, change, counter);
-    int new_array[counter];
-
-    make_new_array(old_array, new_array, n, change);
-    output(new_array, counter);
-
+    int size, notvalid;
+    scanf("%d", &size);
+    scanf("%d", &notvalid);
+    int old_array[size];
+    int newsize = input(old_array, size, notvalid);
+    int new_array[newsize];
+    make_new_array(old_array, new_array, size, notvalid);
+    output(new_array, newsize);
     return 0;
 }
