8b0f694b2199f0ce94cfa0a03b35a14a438e7d78
1rlan <irlanidzzze@gmail.com>
2022-11-17 02:45:48 +0300

    last comments

--- homework №3/clean.s
+++ homework №3/clean.s
@@ -1,7 +1,7 @@
 	.intel_syntax noprefix
+	
 	.text
 	.globl	nextStep
-
 nextStep:
 	push	rbp                                    # Выделяем память под функцию
 	mov	rbp, rsp                                   #
@@ -25,8 +25,8 @@ nextStep:
 	movsd	xmm1, QWORD PTR .LC0[rip]              # xmm1 = 3.0
 	divsd	xmm0, xmm1                             # xmm0 /= xmm1                   | ((2 * prediction) + (n / (prediction * prediction))) / 3.0
 
-	movq	rax, xmm0                              # rax = xmm0 
-	movq	xmm0, rax
+	movq	rax, xmm0                              
+	movq	xmm0, rax                              # xmm0 = valueToReturn
 	pop	rbp
 	ret
 
@@ -41,7 +41,7 @@ root:
 	movsd	xmm0, QWORD PTR -24[rbp]               # xmm0 = number
 	movsd	xmm1, QWORD PTR .LC0[rip]              # xmm1 = 3
 	divsd	xmm0, xmm1                             # xmm0 /= 3
-	movsd	QWORD PTR -8[rbp], xmm0                # previousStep = xmm0  
+	movsd	QWORD PTR -8[rbp], xmm0                # [-8] = previousStep  
 
 	movsd	xmm0, QWORD PTR -24[rbp]               # xmm0 = number
 	mov	rax, QWORD PTR -8[rbp]                     # rax = previousStep
@@ -50,43 +50,51 @@ root:
 	call	nextStep                               # nextStep(xmm0, xmm1)
 	movq	rax, xmm0                              # rax = xmm0
 	mov	QWORD PTR -16[rbp], rax                    # [-16] = step
-
 	jmp	.L4
+
 .L5:
 	movsd	xmm0, QWORD PTR -16[rbp]               # xmm = step
 	movsd	QWORD PTR -8[rbp], xmm0                # previousStep = step
 
-	movsd	xmm0, QWORD PTR -24[rbp]
-	mov	rax, QWORD PTR -8[rbp]
-	movapd	xmm1, xmm0
-	movq	xmm0, rax
-	call	nextStep
-	movq	rax, xmm0
-	mov	QWORD PTR -16[rbp], rax
+	movsd	xmm0, QWORD PTR -24[rbp]               # xmm0 = number
+	mov	rax, QWORD PTR -8[rbp]                     # rax = previousStep
+	movapd	xmm1, xmm0                             # xmm1 = number
+	movq	xmm0, rax                              # xmm0 = previousStep
+	call	nextStep                               # nextStep(previousStep, number)
+
+	movq	rax, xmm0                              
+	mov	QWORD PTR -16[rbp], rax                    # step = valueToReturn
+
 .L4:
-	movsd	xmm0, QWORD PTR -8[rbp]
-	subsd	xmm0, QWORD PTR -16[rbp]
+	movsd	xmm0, QWORD PTR -8[rbp]                # xmm0 = previousStep
+	subsd	xmm0, QWORD PTR -16[rbp]               # xmm0 -= step
+
 	movq	xmm1, QWORD PTR .LC1[rip]
-	andpd	xmm0, xmm1
-	comisd	xmm0, QWORD PTR .LC2[rip]
-	ja	.L5
-	movsd	xmm0, QWORD PTR -16[rbp]
+	andpd	xmm0, xmm1                             # fabs(xmm0)
+
+	comisd	xmm0, QWORD PTR .LC2[rip]              # compare(fabs(xmm0), epsilon)
+	ja	.L5                                        # if (fabs(xmm0) > epsilon) goto .L5
+
+	movsd	xmm0, QWORD PTR -16[rbp]               # xmm0 = step
+
 	movq	rax, xmm0
-	movq	xmm0, rax
+	movq	xmm0, rax                              # xmm0 = valueToReturn (step)
 	leave
 	ret
 
-	.section	.rodata
-.LC3:
-	.string	"%lf"
-.LC5:
-	.string	"%lf\n"
-.LC6:
-	.string	"%d\n"
+
+	.section	.rodata                            #
+.LC3:                                              #
+	.string	"%lf"                                  # 
+.LC5:                                              # Секция констант
+	.string	"%lf\n"                                #
+.LC6:                                              #
+	.string	"%d\n"                                 #
 	
+
+
 	.text
 	.globl	main
-
 main:
 	push	rbp                                    #
 	mov	rbp, rsp                                   # Выделяем память под функцию
@@ -100,49 +108,54 @@ main:
 	call	__isoc99_scanf@PLT                     # scanf(rdi, rsi)
 
 	
-	movsd	xmm0, QWORD PTR -8[rbp]					
+	movsd	xmm0, QWORD PTR -8[rbp]				   # xmm0 = n
+	pxor	xmm1, xmm1                             
+	ucomisd	xmm0, xmm1
+	jp	.L12                                       # if (xmm0 == n) goto .L12
 	pxor	xmm1, xmm1
 	ucomisd	xmm0, xmm1
-	jp	.L12
-	pxor	xmm1, xmm1
-	ucomisd	xmm0, xmm1
-	je	.L8
+	je	.L8                                        # if (xmm0 != n) goto .L8
+
 
 .L12:
-	mov	rax, QWORD PTR -8[rbp]
-	movq	xmm0, rax
-	call	root
-	movq	rax, xmm0
-	movq	xmm0, rax
-	lea	rax, .LC5[rip]
-	mov	rdi, rax
-	mov	eax, 1
-	call	printf@PLT
+	mov	rax, QWORD PTR -8[rbp]                     
+	movq	xmm0, rax                              # xmm0 = n
+	call	root                                   # root(xmm0)
+	movq	rax, xmm0                              # rax = root(xmm0)
+
+	movq	xmm0, rax                              # xmm0 = rax
+	lea	rax, .LC5[rip]                             # rax = "%lf\n"
+	mov	rdi, rax                                   # rdi = "%lf\n"
+	mov	eax, 1                                     #
+	call	printf@PLT                             # print("%lf\n", xmm0)
 	jmp	.L10
+
 .L8:
-	mov	esi, 0
-	lea	rax, .LC6[rip]
-	mov	rdi, rax
-	mov	eax, 0
-	call	printf@PLT
+	mov	esi, 0                                     # esi = 0
+	lea	rax, .LC6[rip]                             # rax = "%d\n" 
+	mov	rdi, rax                                   # rdi = rax
+	mov	eax, 0                                 
+	call	printf@PLT                             # printf("%d\n", 0)
+
 .L10:
 	mov	eax, 0
 	leave
 	ret
 
-	.section	.rodata
-	.align 8
-.LC0:
-	.long	0
-	.long	1074266112
-	.align 16
-.LC1:
-	.long	-1
-	.long	2147483647
-	.long	0
-	.long	0
-	.align 8
-.LC2:
-	.long	-755914244
-	.long	1061184077
+
+	.section	.rodata                            #
+	.align 8                                       #
+.LC0:                                              #
+	.long	0                                      #
+	.long	1074266112                             #
+	.align 16                                      #
+.LC1:                                              #
+	.long	-1                                     # Секция констант
+	.long	2147483647                             #
+	.long	0                                      #
+	.long	0                                      #
+	.align 8                                       #
+.LC2:                                              #
+	.long	-755914244                             #
+	.long	1061184077                             #
 	
