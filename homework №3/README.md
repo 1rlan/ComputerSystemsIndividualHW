42f6c96407c4c6388cda9d1876d9cfdc3f834785
1rlan <irlanidzzze@gmail.com>
2022-11-16 21:52:01 +0300

    add hw3 folder :P

---
+++ homework №3/README.md
@@ -0,0 +1,6 @@
+# Домашнее задание №3
+
+### [Битая ссылка](https://github.com/1rlan/csaihw/blob/master/homework%20%E2/report.md) - отчет о проделанной работе
+
+Прошу заметить, что ассемблерный файл с комментариями и оптимизацией - [Битая ссылка](https://github.com/1rlan/csaihw/blob/master/homework%20%E2%84962/clean.s)\
+Спасибо за проверку!
---
+++ homework №3/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+double nextStep(double prediction, double n) {
+    return (2 * prediction + n / (prediction * prediction)) / 3.0;
+}
+
+double root(double number) {
+    double previousStep = number / 3.0;
+    double step = nextStep(previousStep, number);
+    while (fabs(previousStep - step) > 0.0005) {
+        previousStep = step;
+        step = nextStep(previousStep, number);
+    }
+    return step;
+}
+
+int main() {
+    double n;
+    scanf("%lf", &n);
+    if (n != 0) {
+        printf("%lf\n", root(n));
+    } else {
+        printf(0);
+    }
+    return 0;
+}
