6417100306faf668a02583a2e5fdadc497fe9a92
1rlan <irlanidzzze@gmail.com>
2022-11-17 12:31:49 +0300

    last commentss

--- homework №3/clean.s
+++ homework №3/clean.s
@@ -27,26 +27,20 @@ root:
 	call	nextStep                               # nextStep(xmm0, xmm1)
 	movsd	QWORD PTR -16[rbp], xmm0               # atep = valueToReturn            | step = nextStep(previousStep, number)
 	jmp	.L4
-
 .L5:
-	movsd	xmm0, QWORD PTR -16[rbp]               # xmm = step
+	movsd	xmm0, QWORD PTR -16[rbp]               # xmm0 = step
 	movsd	QWORD PTR -8[rbp], xmm0                # previousStep = step
 	movsd	xmm1, QWORD PTR -24[rbp]               # xmm1 = number
 	call	nextStep                               # nextStep(previousStep, number)
 	movsd	QWORD PTR -16[rbp], xmm0               # step = valueToReturn            | step = nextStep(previousStep, number);
-
 .L4:
 	movsd	xmm0, QWORD PTR -8[rbp]                # xmm0 = previousStep
 	subsd	xmm0, QWORD PTR -16[rbp]               # xmm0 -= step
-
-	movq	xmm1, QWORD PTR .LC1[rip]
-	andpd	xmm0, xmm1                             # fabs(xmm0)
-
+	movq	xmm1, QWORD PTR .LC1[rip]              # xmm1 = 0.0005
+	andpd	xmm0, xmm1                             # fabs((previousStep - step), 0.005)
 	comisd	xmm0, QWORD PTR .LC2[rip]              # compare(fabs(xmm0), epsilon)
 	ja	.L5                                        # if (fabs(xmm0) > epsilon) goto .L5
-
-	movsd	xmm0, QWORD PTR -16[rbp]               # xmm0 = step
-
+	movsd	xmm0, QWORD PTR -16[rbp]               # return step
 	leave
 	ret
 
@@ -55,13 +49,11 @@ main:
 	push	rbp                                    #
 	mov	rbp, rsp                                   # Выделяем память под функцию
 	sub	rsp, 16                                    # 
-
 	lea	rax, .LC3[rip]
 	mov	rdi, rax                                   # rdi = "%lf"
 	lea	rax, -8[rbp]							   
 	mov	rsi, rax                                   # rsi = n
 	call	__isoc99_scanf@PLT                     # scanf("%lf", n)
-
 	movsd	xmm0, QWORD PTR -8[rbp]				   # xmm0 = n
 	pxor	xmm1, xmm1                             
 	ucomisd	xmm0, xmm1
@@ -69,27 +61,20 @@ main:
 	pxor	xmm1, xmm1
 	ucomisd	xmm0, xmm1
 	je	.L8                                        # if (n != 0) goto .L8
-
-
 .L12:                
-	movq	xmm0, QWORD PTR -8[rbp]                # xmm0 = n
 	call	root                                   # root(xmm0)
-
 	lea	rax, .LC5[rip]                             
 	mov	rdi, rax                                   # rdi = "%lf\n"                                 
 	call	printf@PLT                             # print("%lf\n", xmm0)
 	jmp	.L10
-
 .L8:
 	lea	rax, .LC6[rip]                              
 	mov	rdi, rax                                   # rdi = "%d\n"    
 	mov	esi, 0                                     # esi = 0                             
 	call	printf@PLT                             # printf("%d\n", 0)
-
 .L10:
 	leave
 	ret
-
 
 	.section	.rodata                            #
 .LC0:                                              #
